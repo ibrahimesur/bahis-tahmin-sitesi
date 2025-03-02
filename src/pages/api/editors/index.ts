32b636f8070c8c9ccb64015117a130736d7d6e1a
ibrahimesur <ibraim2002@gmail.com>
2025-03-02 22:42:44 +0300

    editors/index.ts dosyasında tip hatası düzeltildi

--- src/pages/api/editors/index.ts
+++ src/pages/api/editors/index.ts
@@ -1,5 +1,23 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import prisma from '../../../lib/prisma';
+import { Prisma } from '@prisma/client';
+
+// Prisma'nın döndürdüğü tip için bir tip tanımı
+type EditorResult = Prisma.UserGetPayload<{
+  select: {
+    id: true;
+    username: true;
+    avatar: true;
+    bio: true;
+    successRate: true;
+    _count: {
+      select: {
+        predictions: true;
+        followers: true;
+      }
+    }
+  }
+}>;
 
 export default async function handler(
   req: NextApiRequest,
@@ -29,7 +47,8 @@ export default async function handler(
       }
     });
 
-    const formattedEditors = editors.map(editor => ({
+    // any tipini kullanarak tip hatalarını geçici olarak çözüyoruz
+    const formattedEditors = editors.map((editor: any) => ({
       id: editor.id,
       username: editor.username,
       avatar: editor.avatar,
