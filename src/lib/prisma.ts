acef6967b50ebfec41dfd31d894617806245671e
ibrahimesur <ibraim2002@gmail.com>
2025-03-03 23:35:12 +0300

    Netlify build komutunu güncellendi

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -6,6 +6,11 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient };
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
+    datasources: {
+      db: {
+        url: "postgresql://neondb_owner:npg_csYWLdx4zgG9@ep-polished-shadow-a8x4j8y3-pooler.eastus2.azure.neon.tech/neondb?sslmode=require"
+      }
+    },
     log: ['query', 'error', 'warn'],
   });
 
