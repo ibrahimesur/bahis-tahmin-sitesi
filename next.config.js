d46fe74533936a46cdc7ca7c8d73b7f411a0375c
ibrahimesur <ibraim2002@gmail.com>
2025-03-05 00:20:17 +0300

    Netlify ve Next.js yapılandırmaları eski haline getirildi

--- next.config.js
+++ next.config.js
@@ -3,23 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: [
-      'media-4.api-sports.io',
-      'media-3.api-sports.io',
-      'media-2.api-sports.io',
-      'media-1.api-sports.io',
-      'media.api-sports.io',
-      'crests.football-data.org',
-      'upload.wikimedia.org',
-      'tmssl.akamaized.net',
-      'img.uefa.com',
-      'www.flashscore.com',
-      'www.sofascore.com'
-    ],
-    unoptimized: process.env.NODE_ENV === 'development',
+    domains: ['via.placeholder.com', 'images.unsplash.com'],
+    unoptimized: true,
   },
   trailingSlash: true,
-  output: 'standalone', // Netlify için output ayarı
+  output: 'export', // Statik site dışa aktarma modu
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
