d88fa9c729f091acc9d00ce3c78f362fbb36aeb3
ibrahimesur <ibraim2002@gmail.com>
2025-03-05 00:07:45 +0300

    Next.js yapılandırması Netlify için optimize edildi

--- next.config.js
+++ next.config.js
@@ -3,11 +3,23 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['via.placeholder.com', 'images.unsplash.com'],
-    unoptimized: true,
+    domains: [
+      'media-4.api-sports.io',
+      'media-3.api-sports.io',
+      'media-2.api-sports.io',
+      'media-1.api-sports.io',
+      'media.api-sports.io',
+      'crests.football-data.org',
+      'upload.wikimedia.org',
+      'tmssl.akamaized.net',
+      'img.uefa.com',
+      'www.flashscore.com',
+      'www.sofascore.com'
+    ],
+    unoptimized: process.env.NODE_ENV === 'development',
   },
   trailingSlash: true,
-  output: 'export', // Statik site dışa aktarma modu
+  output: 'standalone', // Netlify için output ayarı
 }
 
 module.exports = nextConfig
